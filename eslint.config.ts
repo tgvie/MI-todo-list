fc03dd7526aca20ca96bff6e1a2c4697e7b8fdfd
tgvie <166875380+tgvie@users.noreply.github.com>
2025-01-30 00:51:34 +0100

    add rules and install sass

--- eslint.config.ts
+++ eslint.config.ts
@@ -11,6 +11,31 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+
+    // Custom rules
+    rules: {
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'block-spacing': 'error',
+      'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+      'comma-dangle': ['error', 'always-multiline'],
+      curly: ['error', 'all'],
+      'import/no-absolute-path': 'off',
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'key-spacing': ['error', { afterColon: true, beforeColon: false }],
+      'keyword-spacing': ['error', { before: true }],
+      'no-console': process.env.PROD ? 'warn' : 'off',
+      'no-debugger': process.env.PROD ? 'warn' : 'off',
+      'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+      'no-undef': process.env.PROD ? 'error' : 'warn',
+      'no-unreachable': process.env.PROD ? 'error' : 'warn',
+      'no-unused-vars': process.env.PROD ? 'error' : 'warn',
+      'object-curly-spacing': ['error', 'always'],
+      "quotes": ["error", "single", { "avoidEscape": true }],
+      semi: ['error', 'always'],
+      'space-before-blocks': ['error', 'always'],
+      'space-infix-ops': ['error', { int32Hint: false }],
+      "eol-last": ["error", "never"],
+    },
   },
 
   {
