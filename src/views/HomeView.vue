d76ed1f40da1cca1f5abf41d86ea911b10ff3b1e
tgvie <166875380+tgvie@users.noreply.github.com>
2025-01-30 18:24:21 +0100

    add feature to print out todo objects

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,5 +13,15 @@
   <main>
     <input type="text" v-model="todoName" @keyup.enter="addTodo">
     <button @click="addTodo">+</button>
+    <div>
+      <p v-if="todos.length === 0">You’re free as a bird! 🕊️
+        <br>Time to relax or do something fun! 🎉</p>
+        
+      <ul v-if="todos.length > 0">
+        <li v-for="(todo, index) in todos" :key="index">
+          {{  todo }}
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
