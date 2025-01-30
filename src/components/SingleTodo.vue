b68a51419168f23ba7ba34ec81f1a7eb4502cdb2
tgvie <166875380+tgvie@users.noreply.github.com>
2025-01-30 18:49:40 +0100

    add feature to single out every todo task to mark them as done/undone individually

---
+++ src/components/SingleTodo.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const props = defineProps({
+    todoText: { type: String, required: true },
+    complete: { type: Boolean, default: false}
+});
+
+const isDone = ref(props.complete);
+
+function toggleDoneState() {
+    isDone.value = !isDone.value;
+}
+</script>
+
+<template>
+  <div :class="{ completed: isDone }">
+    {{ todoText }}
+    <button @click="toggleDoneState">
+        <span v-if="isDone">Undone</span>
+        <span v-if="!isDone">Done</span>
+    </button>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,29 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
+import '../assets/style.scss';
+import { ref } from 'vue';
+import SingleTodo from '@/components/SingleTodo.vue';
 
-  const todos = ref<string[]>([]); //Empty array of todo list
-  const todoName = ref(''); //User's todo input
+const todos = ref<string[]>([]); //Empty array of todo list
 
-  function addTodo() {
-    todos.value.push(todoName.value);
-  }
+const todoName = ref(''); //User's todo input
+
+function addTodo() {
+  todos.value.push(todoName.value);
+}
 </script>
 
 <template>
   <main>
-    <input type="text" v-model="todoName" @keyup.enter="addTodo">
+    <input type="text" v-model="todoName" @keyup.enter="addTodo" />
     <button @click="addTodo">+</button>
     <div>
       <p v-if="todos.length === 0">You’re free as a bird! 🕊️
         <br>Time to relax or do something fun! 🎉</p>
-        
-      <ul v-if="todos.length > 0">
-        <li v-for="(todo, index) in todos" :key="index">
-          {{  todo }}
-        </li>
-      </ul>
+
+      <!-- eslint-disable-next-line vue/no-use-v-if-with-v-for -->
+      <div v-if="todos.length > 0" v-for="(todo, index) in todos" :key="index">
+        <SingleTodo :todo-text="todo"/>
+      </div>
     </div>
   </main>
 </template>
