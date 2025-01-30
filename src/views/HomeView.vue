a5d4ad6faa2b186817709bb45c5e615eb68fc8c7
tgvie <166875380+tgvie@users.noreply.github.com>
2025-01-30 04:50:21 +0100

    update various files and devs, add base for to do list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const todos = ref<string[]>([]); //Empty array of todo list
+  const todoName = ref(''); //User's todo input
+
+  function addTodo() {
+    todos.value.push(todoName.value);
+  }
+</script>
 
 <template>
-  <main>Main</main>
+  <main>
+    <input type="text" v-model="todoName" @keyup.enter="addTodo">
+    <button @click="addTodo">+</button>
+  </main>
 </template>
